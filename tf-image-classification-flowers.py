ad74b19a835857960f921f312672731b3830383b
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2022-10-06 00:07:33 -0700

    data sampling. dataset creation.

--- tf-image-classification-flowers.py
+++ tf-image-classification-flowers.py
@@ -67,4 +67,63 @@ PIL.Image.open(str(tulips[0])).show()
 PIL.Image.open(str(tulips[1])).show()
 
 
+log_phase(f"PHASE 2:  Create datasets. ")
+
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+log(f"training_dataset: image_dataset_from_directory - validation_split=0.2: {dataset_dir}")
+training_dataset = tf.keras.utils.image_dataset_from_directory(
+    dataset_dir,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
+
+
+log(f"validation_dataset: image_dataset_from_directory - validation_split=0.2: {dataset_dir}")
+validation_dataset = tf.keras.utils.image_dataset_from_directory(
+    dataset_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size
+)
+
+class_names = training_dataset.class_names
+log(f"Class names (class_names):\n{class_names}")
+
+
+log_phase(f"PHASE 3:  Examine first 9 images of the dataset. ")
+
+log(f"PLOT:  3x3 - sample of 9 images with labels/categories/classes. ")
+plt.figure(figsize=(10, 10))
+for images, labels in training_dataset.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+
+plt.show()
+
+
+# Below is text from the tutorial. TODO: Clarify the "retrieve batches" aspect.
+# The little disabled block of code returns a few tuples and stops.
+# You will pass these datasets to the Keras Model.fit method for training later in this tutorial.
+# If you like, you can also manually iterate over the dataset and retrieve batches of images:
+# for image_batch, labels_batch in training_dataset:
+#     print(image_batch.shape)
+#     print(labels_batch.shape)
+#     break
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+training_dataset = training_dataset.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.cache().prefetch(buffer_size=AUTOTUNE)
 
